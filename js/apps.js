bfb9a263413d437556e1b69f946034bf38518e6a
jmscoding100 <joshua.z.mcgee@gmail.com>
2024-12-04 13:09:49 -0600

    list-3

--- js/apps.js
+++ js/apps.js
@@ -15,12 +15,15 @@ const completedBtn = document.getElementById('completedBtn')
 const completedList = document.getElementById('completedList')
 const completedTask = document.getElementById('completedTask')
 
+
+//testing make task item
+let prevItem
+let currentItem
+
 //array of task
 let taskArry = []
 
-let task ={
-
-}
+let task ={}
 
 //addTaskBtn
 addTaskBtn.addEventListener('click', (e)=>{
@@ -30,12 +33,26 @@ addTaskBtn.addEventListener('click', (e)=>{
 
 //take input
 const validateInput =()=> {
-
+    // testing
+    
     if (taskInput.value === '') {
         alert('Please enter a task before submmiting')
     } else {
+
+        for (let i = 0; i < taskArry.length; i++) {
+            if (taskInput.value == taskArry[i].task){
+                alert('Task has already been added')
+                taskInput.value = ''
+                return
+            }
+        }
         makeTask(taskInput.value)
     }
+
+// end test ...Good
+
+
+    taskInput.value = ''
 }
 
 //make task
@@ -71,13 +88,26 @@ const makeTaskItem =(el, item)=>{
     const checkbox = document.createElement('input')
     checkbox.setAttribute('type', 'checkbox')
     checkbox.setAttribute('id',`taskId-${item.id}`)
+    checkbox.setAttribute('data-id', `${item.id}`)
     checkbox.classList.add('form-check-input','checkbox')
 
     const label = document.createElement('label')
     label.setAttribute('for', `taskId-${item.id}`)
     label.classList.add('form-check-label', 'text-capitalize', 'mx-2', 'task-label')
-    label.innerText =`${item.id} - Date Created ${item.dateAdded}`
 
+    // label.innerText =`${item.task} - Date Created ${item.dateAdded}`
+    const taskSpan = document.createElement('span')
+    taskSpan.classList.add('task-span')
+    taskSpan.innerText = item.task
+
+
+    const dateAddedSpan = document.createElement('span')
+    dateAddedSpan.classList.add('date-added')
+    dateAddedSpan.innerText = ` | ${item.dateAdded}`
+
+
+label.appendChild(taskSpan)
+label.appendChild(dateAddedSpan)
 
     li.appendChild(checkbox)
     li.appendChild(label)
@@ -88,13 +118,67 @@ const makeTaskItem =(el, item)=>{
 
 completedBtn.addEventListener('click', (e)=>{
     e.preventDefault()
-    console.log('click')
+    // console.log('click')
+    validateCompletedTask()
 })
 
 //validate checked task
 
 const validateCompletedTask =()=>{
+
     let completedArr = []
     const checkboxes = document.querySelectorAll('.checkbox')
-    const allTasks = document.querySelectorAll('.task-label')
+
+
+    //testing
+    for(let i=0; i<checkboxes.length; i++) {
+        if(checkboxes[i].checked && (checkboxes[i].getAttribute('data-id') == taskArry[i].id)) {
+            taskArry[i] ={
+                ...taskArry[i],
+                isCompleted: true,
+                dateCompleted: new Date().toString()
+            }
+        }
+    }
+
+    console.log(taskArry)
+
+
+
+    for(let i = 0; i < taskArry.length; i++){
+        if (taskArry[i].isCompleted) {
+            completedArr = [...completedArr, taskArry[i]]
+        }
+    }
+    //end testing
+
+
+
+    completedTask.innerText = completedArr.length
+    makeCompleteItem(completedArr)
+
+}
+
+
+
+//make li for completed list
+const makeCompleteItem = (arr)=> {
+    arr.forEach(item => {
+        const task = item.task
+        const dateCompleted = item.dateCompleted
+
+
+        const completedItem = document.createElement('li')
+        completedItem.classList.add('list-group-item', 'text-success', 'text-capitalize', 'completed-item')
+        completedItem.innerText = `${task} | completed: ${dateCompleted}`
+
+        //testing
+        currentItem = task
+
+        if(currentItem !== prevItem) {
+            completedList.appendChild(completedItem)
+            prevItem = currentItem
+            return
+        }
+    })
 }
